2f8af53fcc34676eed744f95d663b5571cfa2769
shohan <shohan@therapservices.net>
2021-06-09 16:47:53 +0600

    Initial project structure

---
+++ src/main/java/com/innolytic/ssp/SspApplication.java
@@ -0,0 +1,13 @@
+package com.innolytic.ssp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/innolytic/ssp/SspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innolytic.ssp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
